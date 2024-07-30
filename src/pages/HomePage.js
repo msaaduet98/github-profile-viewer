79e698e9bd6a5c2d1e1f5e960039b24e85208009
Muhammad Arslan <m.arslanijaz9@gmail.com>
2024-07-30 23:35:42 +0500

    initial code

---
+++ src/pages/HomePage.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import Card from '../components/Card';
+import './HomePage.css';
+
+const HomePage = () => {
+  const [query, setQuery] = useState('');
+  const [users, setUsers] = useState([]);
+
+  const handleSearch = async () => {
+    const response = await axios.get(`https://api.github.com/search/users?q=${query}`);
+    setUsers(response.data.items);
+  };
+
+  return (
+    <div className="home-page">
+      <input
+        type="text"
+        placeholder="Search for GitHub profiles..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+      <div className="cards-container">
+        {users.map(user => (
+          <Card key={user.id} user={user} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
